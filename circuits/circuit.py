7a49ed9317f00a105da20f6cae3c849010446697
smoksde <smoksde@gmail.com>
2025-08-30 08:46:41 +0200

    correct group handling in square and multiply

--- circuits/circuit.py
+++ circuits/circuit.py
@@ -231,33 +231,35 @@ def initial_approximation(circuit, m_bits, n):
 
 
 def modular_exponentiation(circuit, base, exponent, modulus, parent_group=None):
-    me_group = circuit.add_group("MODULAR_EXPONENTIATION")
-    me_group.set_parent(parent_group)
+    this_group = circuit.add_group("MODULAR_EXPONENTIATION")
+    this_group_id = this_group.id if this_group is not None else -1
+    if circuit.enable_groups and this_group is not None:
+        this_group.set_parent(parent_group)
     n = len(base)
     assert n == len(exponent) and n == len(
         modulus
     ), "All input must have the same bit length"
 
-    zero = constant_zero(circuit, base[0], parent_group=me_group)
-    one = constant_one(circuit, base[0], parent_group=me_group)
+    zero = constant_zero(circuit, base[0], parent_group=this_group)
+    one = constant_one(circuit, base[0], parent_group=this_group)
 
     result = [zero] * n
     result[0] = one
-    base_mod = modulo_circuit(circuit, base, modulus, parent_group=me_group)
+    base_mod = modulo_circuit(circuit, base, modulus, parent_group=this_group)
     for i in range(n):
         bit_pos = n - 1 - i
         current_bit = exponent[bit_pos]
         squared = wallace_tree_multiplier(
-            circuit, result, result, parent_group=me_group
+            circuit, result, result, parent_group=this_group
         )
         squared = squared[: len(base)]
-        squared_mod = modulo_circuit(circuit, squared, modulus, parent_group=me_group)
+        squared_mod = modulo_circuit(circuit, squared, modulus, parent_group=this_group)
         with_multiply = wallace_tree_multiplier(
-            circuit, squared_mod, base_mod, parent_group=me_group
+            circuit, squared_mod, base_mod, parent_group=this_group
         )
         with_multiply = with_multiply[: len(base)]
         multiply_mod = modulo_circuit(
-            circuit, with_multiply, modulus, parent_group=me_group
+            circuit, with_multiply, modulus, parent_group=this_group
         )
         new_result = [None] * n
         for j in range(n):
@@ -265,25 +267,25 @@ def modular_exponentiation(circuit, base, exponent, modulus, parent_group=None):
                 "not",
                 f"NOT_BIT_{bit_pos}_{j}",
                 inputs=[current_bit],
-                group_id=me_group.id,
+                group_id=this_group_id,
             ).ports[1]
             and1 = circuit.add_node(
                 "and",
                 f"AND_MULT_{bit_pos}_{j}",
                 inputs=[current_bit, multiply_mod[j]],
-                group_id=me_group.id,
+                group_id=this_group_id,
             ).ports[2]
             and2 = circuit.add_node(
                 "and",
                 f"AND_SQR_{bit_pos}_{j}",
                 inputs=[not_bit, squared_mod[j]],
-                group_id=me_group.id,
+                group_id=this_group_id,
             ).ports[2]
             new_result[j] = circuit.add_node(
                 "or",
                 f"OR_RESULT_{bit_pos}_{j}",
                 inputs=[and1, and2],
-                group_id=me_group.id,
+                group_id=this_group_id,
             ).ports[2]
         result = new_result
     return result
